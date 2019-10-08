820899e96f1018c38ee616d8d5dcd233539e382a
Venera <venera_husainova@mail.ru>
2019-10-08 17:25:06 +0300

    доработки

--- js/main.js
+++ js/main.js
@@ -39,7 +39,8 @@ var createElementPin = function (number) {
 };
 var sortFeatureElement = function (element, arr, index) {
   for (var k = arr[index].offer.features.length; k < FEATURES.length; k++) {
-    return element[k].style.display = 'none';
+    element[k].style.display = 'none'
+    return element[k];
   }
 };
 var translateType = function (type) {
