7d782ba9255a8de7b9a8dfc714b20cdb159e2382
Venera <venera_husainova@mail.ru>
2019-10-22 10:38:30 +0300

    работа над ошибками

--- js/card.js
+++ js/card.js
@@ -1,35 +1,36 @@
 'use strict';
 (function () {
   var cardTemplateElement = document.querySelector('#card').content.querySelector('.map__card');
-  var fragmentCard = document.createDocumentFragment();
+  var cardElementClone = cardTemplateElement.cloneNode(true);
+  var fragment = document.createDocumentFragment();
 
-  var sortFeatureElement = function (element, arr, index) {
-    for (var k = arr[index].offer.features.length; k < window.variables.FEATURES.length; k++) {
-      element[k].style.display = 'none';
-    }
-    return element[k];
+  var createFeatureElement = function (feature, wrapper) {
+    var classElement = '.popup__feature--' + feature;
+    var elementClone = cardTemplateElement.querySelector(classElement);
+    var element = elementClone.cloneNode(true);
+    wrapper.appendChild(element);
   };
   var translateType = function (type) {
-    var nameType;
-    switch (type) {
-      case ('house'):
-        nameType = 'Дом';
-        break;
-      case ('flat'):
-        nameType = 'Квартира';
-        break;
-      case ('palace'):
-        nameType = 'Дворец';
-        break;
-      case ('bungalo'):
-        nameType = 'Бунгало';
-        break;
+  var typeRoom = {
+      'house': 'Дом',
+        'flat': 'Квартира',
+        'palace': 'Дворец',
+        'bungalo': 'Бунгало'
     }
+    var nameType = typeRoom[type];
     return nameType;
   };
-  var createElementCard = function (arr, num) {
-    var element = cardTemplateElement.cloneNode(true);
-    var featuresElement = element.querySelectorAll('.popup__feature');
+  var createPhoto = function (element) {
+    for (var j = 0; j < window.data.PHOTOS.length; j++) {
+      var photo = cardTemplateElement.querySelector('.popup__photo').cloneNode(true);
+      photo.src = window.data.PHOTOS[j];
+      element.querySelector('.popup__photos').appendChild(photo);
+    }
+  };
+  var createElementCard = function (element, arr, num) {
+    var wrapperFeature = element.querySelector('.popup__features');
+    var featureElement = element.querySelectorAll('.popup__feature');
+    var photoElement = element.querySelectorAll('.popup__photo');
     element.querySelector('.popup__avatar').src = arr[num].author.avatar;
     element.querySelector('.popup__avatar').alt = arr[num].offer.title;
     element.querySelector('.popup__text--address').textContent = arr[num].offer.address();
@@ -39,26 +40,23 @@
     element.querySelector('.popup__text--capacity').textContent = arr[num].offer.rooms + ' комнаты для ' + arr[num].offer.guests + ' гостей';
     element.querySelector('.popup__text--time').textContent = 'Заезд после' + arr[num].offer.checkin + ', выезд до ' + arr[num].offer.checkout;
     element.querySelector('.popup__description').textContent = arr[num].offer.description;
-    element.querySelector('.popup__photo').src = window.variables.PHOTOS[0];
-    var createPhoto = function () {
-      for (var j = 1; j < window.variables.PHOTOS.length; j++) {
-        var photo = element.querySelector('.popup__photo').cloneNode(true);
-        photo.src = window.variables.PHOTOS[j];
-        element.querySelector('.popup__photos').appendChild(photo);
-      }
-    };
-    createPhoto();
-    sortFeatureElement(featuresElement, arr, num);
-    return element;
+    Array.from(featureElement).forEach(function (item) {
+      item.parentNode.removeChild(item);
+    });
+    arr[num].offer.features.forEach(function (item) {
+      createFeatureElement(item, wrapperFeature);
+    });
+    Array.from(photoElement).forEach(function (item) {
+      item.parentNode.removeChild(item);
+    });
+    createPhoto(element);
+    element.classList.add('hidden');
   };
-  for (var j = 0; j < window.variables.AMOUNT_OFFER; j++) {
-    var cardElement = createElementCard(window.data.dateForRoom, j);
-    var cardElements = [];
-    cardElements.push(cardElement);
-    fragmentCard.appendChild(cardElement);
-  }
-  window.variables.mapElement.appendChild(fragmentCard);
+  createElementCard(cardElementClone, window.data.dataForRooms, 0)
+  fragment.appendChild(cardElementClone);
+  window.variables.mapElement.appendChild(fragment);
   window.card = {
-    cardElements: cardElements
+    createElementCard: createElementCard
   };
 })();
+
